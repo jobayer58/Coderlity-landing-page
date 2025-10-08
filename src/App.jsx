66698e597b8433b6c41fcba940b2ae0e48aa46db
jobayer58 <jobayerafnan770@gmail.com>
2025-10-08 17:49:08 +0600

    added top product section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { MdDomain } from "react-icons/md"
 import image1 from '../src/assets/iamges/image1.png'
 import image2 from '../src/assets/iamges/image2.png'
 import image3 from '../src/assets/iamges/image3.png'
+import { FaStar } from "react-icons/fa"
 
 
 function App() {
@@ -31,33 +32,51 @@ function App() {
             <div className="card1">
               <img src={image1} alt="" />
               <div className="p-3 ">
-                <h4>SignalLab - Forex And Crypto <br /> Trading Signal Platform</h4>
-                <p>Ratting 5/5</p>
-                <p>category: Laravel</p>
+                <h4 className="fw-bold">SignalLab - Forex And Crypto <br /> Trading Signal Platform</h4>
+                <p className="d-flex align-items-center gap-2">Ratting 5/5 <div className="d-flex">
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                </div></p>
+                <p>category: <span className="fw-bold">Laravel</span></p>
                 <hr />
-                <p>Live Preview:  $49</p>
+                <p className="d-flex justify-content-between align-items-center liveText">Live Preview:  <span className="dollarSize">$49</span></p>
               </div>
             </div>
 
             <div className="card1">
               <img src={image2} alt="" />
               <div className="p-3">
-                <h4>Xcash - Ultimate Wallet <br /> Solution</h4>
-                <p>Ratting 5/5</p>
-                <p>category: Laravel</p>
+                <h4 className="fw-bold">Xcash - Ultimate Wallet <br /> Solution</h4>
+                <p className="d-flex align-items-center gap-2">Ratting 5/5 <div className="d-flex">
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                </div></p>
+                <p>category: <span className="fw-bold">Laravel</span></p>
                 <hr />
-                <p>Live Preview:  $149</p>
+                <p className="d-flex justify-content-between align-items-center liveText">Live Preview:  <span className="dollarSize">$149</span></p>
               </div>
             </div>
 
             <div className="card1">
               <img src={image3} alt="" />
               <div className="p-3">
-                <h4>HYIPLAB - Complete HYIP <br /> Investment System</h4>
-                <p>Ratting 5/5</p>
-                <p>category: Laravel</p>
+                <h4 className="fw-bold">HYIPLAB - Complete HYIP <br /> Investment System</h4>
+                <p className="d-flex align-items-center gap-2">Ratting 5/5 <div className="d-flex">
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                  <FaStar color="#FFAE00" />
+                </div></p>
+                <p>category: <span className="fw-bold">Laravel</span></p>
                 <hr />
-                <p>Live Preview:  $199</p>
+                <p className="d-flex justify-content-between align-items-center liveText">Live Preview:  <span className="dollarSize ">$199</span></p>
               </div>
             </div>
 
