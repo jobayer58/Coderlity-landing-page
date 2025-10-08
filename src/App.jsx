4ed69477cfaba6b811725445ba4d0514d45a7373
jobayer58 <jobayerafnan770@gmail.com>
2025-10-08 15:34:38 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { MdDomain } from "react-icons/md"
+
+
+function App() {
+
+  return (
+    <main>
+      {/* domain purchase section */}
+      <section>
+        <div>
+          
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default App
