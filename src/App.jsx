9ad434fe75d7dd78c44cfb46aa36936df57e383b
jobayer58 <jobayerafnan770@gmail.com>
2025-10-08 18:43:36 +0600

    complete top product section

--- src/App.jsx
+++ src/App.jsx
@@ -25,58 +25,60 @@ function App() {
           {/* text */}
           <div className="text-center py-5">
             <h1>Our Top Products</h1>
-            <p className="font-color">We offer a range of high-quality digital products, including PHP Laravel frameworks, HTML React JS <br /> front-end templates, and MERN projects, designed to meet the needs of modern web development.</p>
+            <p style={{ color: '#555555' }}>We offer a range of high-quality digital products, including PHP Laravel frameworks, HTML React JS <br /> front-end templates, and MERN projects, designed to meet the needs of modern web development.</p>
           </div>
-          {/* image card 1 */}
+          {/* image card*/}
           <div className=" d-flex gap-4 card-div">
+            {/* card 1 */}
             <div className="card1">
               <img src={image1} alt="" />
               <div className="p-3 ">
-                <h4 className="fw-bold">SignalLab - Forex And Crypto <br /> Trading Signal Platform</h4>
-                <p className="d-flex align-items-center gap-2">Ratting 5/5 <div className="d-flex">
+                <h4 className="fw-bold lh-base">SignalLab - Forex And Crypto <br /> Trading Signal Platform</h4>
+                <div className="d-flex align-items-center gap-2 mb-0">Ratting: <span className="fw-bold">5/5 </span> 
+                <div className="d-flex">
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
-                </div></p>
-                <p>category: <span className="fw-bold">Laravel</span></p>
-                <hr />
-                <p className="d-flex justify-content-between align-items-center liveText">Live Preview:  <span className="dollarSize">$49</span></p>
+                </div></div>
+                <p>Category: <span className="fw-bold">Laravel</span></p>
+                <hr className="my-1" />
+                <p className="d-flex justify-content-between align-items-center" style={{ color: '#FF946A' }}>Live Preview:  <span className="dollarSize">$49</span></p>
               </div>
             </div>
-
+            {/* card 2 */}
             <div className="card1">
               <img src={image2} alt="" />
               <div className="p-3">
-                <h4 className="fw-bold">Xcash - Ultimate Wallet <br /> Solution</h4>
-                <p className="d-flex align-items-center gap-2">Ratting 5/5 <div className="d-flex">
+                <h4 className="fw-bold lh-base">Xcash - Ultimate Wallet <br /> Solution</h4>
+                <p className="d-flex align-items-center gap-2 mb-0">Ratting: <span className="fw-bold">5/5 </span> <div className="d-flex">
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
                 </div></p>
-                <p>category: <span className="fw-bold">Laravel</span></p>
-                <hr />
-                <p className="d-flex justify-content-between align-items-center liveText">Live Preview:  <span className="dollarSize">$149</span></p>
+                <p>Category: <span className="fw-bold">Laravel</span></p>
+                <hr className="my-1" />
+                <p className="d-flex justify-content-between align-items-center " style={{ color: '#FF946A' }}>Live Preview:  <span className="dollarSize">$149</span></p>
               </div>
             </div>
-
+          {/* card 3 */}
             <div className="card1">
               <img src={image3} alt="" />
               <div className="p-3">
-                <h4 className="fw-bold">HYIPLAB - Complete HYIP <br /> Investment System</h4>
-                <p className="d-flex align-items-center gap-2">Ratting 5/5 <div className="d-flex">
+                <h4 className="fw-bold lh-base">HYIPLAB - Complete HYIP <br /> Investment System</h4>
+                <p className="d-flex align-items-center gap-2 mb-0">Ratting: <span className="fw-bold">5/5 </span><div className="d-flex">
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
                   <FaStar color="#FFAE00" />
                 </div></p>
-                <p>category: <span className="fw-bold">Laravel</span></p>
-                <hr />
-                <p className="d-flex justify-content-between align-items-center liveText">Live Preview:  <span className="dollarSize ">$199</span></p>
+                <p>Category: <span className="fw-bold">Laravel</span></p>
+                <hr className="my-1" />
+                <p className="d-flex justify-content-between align-items-center " style={{ color: '#FF946A' }}>Live Preview:  <span className="dollarSize ">$199</span></p>
               </div>
             </div>
 
