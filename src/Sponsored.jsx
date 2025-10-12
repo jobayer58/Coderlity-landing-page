0a1da497fa9e3bb14da66a735058f8fb10d72c64
jobayer58 <jobayerafnan770@gmail.com>
2025-10-12 20:23:42 +0600

    complete domain responsive

--- src/Sponsored.jsx
+++ src/Sponsored.jsx
@@ -8,10 +8,12 @@ import sponsored6 from '../src/assets/iamges/sponsored6.png'
 import sponsored7 from '../src/assets/iamges/sponsored7.png'
 import { Col, Container, Row } from 'react-bootstrap';
 import './Sponsored.css'
+import Marquee from 'react-fast-marquee';
 
 const Sponsored = () => {
-    return (
-         <section className="sponsored-section">
+  return (
+    <section className="sponsored-section">
+      <Marquee>
         <Container fluid className=" sponsored-container">
           <Row className="sponsored-row">
             <Col xs={4} sm={3} md={2} lg="auto" className="text-center">
@@ -32,13 +34,14 @@ const Sponsored = () => {
             <Col xs={4} sm={3} md={2} lg="auto" className="text-center">
               <img src={sponsored6} alt="sponsor" className="img-fluid" />
             </Col>
-            <Col xs={4} sm={3} md={2} lg="auto" className="text-center">
+            {/* <Col xs={4} sm={3} md={2} lg="auto" className="text-center">
               <img src={sponsored7} alt="sponsor" className="img-fluid" />
-            </Col>
+            </Col> */}
           </Row>
         </Container>
-      </section>
-    );
+      </Marquee>
+    </section>
+  );
 };
 
 export default Sponsored;
