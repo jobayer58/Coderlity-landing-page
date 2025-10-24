5e8ccb66988ac40ae1579ea65281f38bd5eb7745
jobayer58 <jobayerafnan770@gmail.com>
2025-10-24 19:30:29 +0600

    update

--- src/Featured.jsx
+++ src/Featured.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import UXImage from '../src/assets/iamges/featured3.png'
-import hostingImage from '../src/assets/iamges/featured1.png'
-import webImage from '../src/assets/iamges/featured4.png'
-import marketingImage from '../src/assets/iamges/featured2.png'
+import UXImage from '../src/assets/iamges/featured5.png'
+import hostingImage from '../src/assets/iamges/featured6.png'
+import webImage from '../src/assets/iamges/featured7.png'
+import marketingImage from '../src/assets/iamges/featured8.png'
 import { Card, Col, Container, Row } from 'react-bootstrap';
 import './Featured.css'
 
@@ -11,40 +11,40 @@ const Featured = () => {
     return (
         <section className='featured-section'>
             <div className='featured-text'>
-                <h1>Featured Items</h1>
-                <p>Explore our top-selling digital products, including custom PHP Laravel frameworks, sleek HTML React JS front-end templates, and <br className='br' /> robust MERN projects. These featured items are designed to offer cutting-edge solutions for modern web development needs.</p>
+                <h1 className='featured-text-title'>Featured Items</h1>
+                <p>We offer a range of high-quality digital products, including PHP Laravel frameworks, HTML React JS <br /> front-end templates, and MERN projects, designed to meet the needs of modern web development.</p>
             </div>
             <div className='featured-card-parent'>
                 {/* card 1 */}
-                <div className='featured-card-div'>
+                <div className='featured-card-div1'>
                     <div className='featured-img'><img src={UXImage} className='img' alt="" /></div>
                     <div className='featured-content-text'>
                         <h1>UI/UX Design</h1>
-                        <p>UI/UX Design services craft engaging, user-friendly digital experiences that blend aesthetics with functionality.</p>
+                        <p>UI/UX Design services craft engaging, user-friendly digital <br /> experiences that blend aesthetics with functionality.</p>
                     </div>
                 </div>
                 {/* card 2 */}
-                <div className='featured-card-div'>
+                <div className='featured-card-div2'>
                     <div className='featured-img'><img src={hostingImage} className='img' alt="" /></div>
                     <div className='featured-content-text'>
                         <h1>Domain Hosting</h1>
-                        <p>Our Domain Hosting service provides secure, reliable, and fast hosting solutions to keep your website always accessible.</p>
+                        <p>Our Domain Hosting service provides secure, reliable, and fast <br />  hosting solutions to keep your website always accessible.</p>
                     </div>
                 </div>
                 {/* card 3 */}
-                <div className='featured-card-div'>
+                <div className='featured-card-div3'>
                     <div className='featured-img'><img src={webImage} className='img' alt="" /></div>
                     <div className='featured-content-text'>
                         <h1>Web / App Developing</h1>
-                        <p>Our Game Design service creates immersive, interactive, and engaging gaming experiences tailored to captivate players.</p>
+                        <p>Our Game Design service creates immersive, interactive, br and engaging gaming experiences tailored to captivate players.</p>
                     </div>
                 </div>
                 {/* card 1 */}
-                <div className='featured-card-div'>
+                <div className='featured-card-div4'>
                     <div className='featured-img'><img src={marketingImage} className='img' alt="" /></div>
                     <div className='featured-content-text'>
                         <h1>Digital Marketing</h1>
-                        <p>Our Digital Marketing service helps grow your brand online through strategic campaigns, SEO, social media, and targeted advertising.</p>
+                        <p>Our Digital Marketing service helps grow your brand online through <br /> strategic campaigns, SEO, social media, and targeted advertising.</p>
                     </div>
                 </div>
             </div>
